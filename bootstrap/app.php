03e0e89c0601b50af35d59d561e9b59ac4b3937d
Michell Gabriel Lucas <gabriellucas5342@gmail.com>
2026-04-25 23:15:15 -0600

    panel de profesor

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
-
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
